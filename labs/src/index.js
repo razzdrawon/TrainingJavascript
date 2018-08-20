d6d64d55a4ba8658cf3e378007a1a69492005be8
Ricardo Axel Padilla Gonzalez <Ricardo.Padilla@walmart.com>
2018-08-20 11:53:19 -0500

    printing random secuence

--- labs/src/index.js
+++ labs/src/index.js
@@ -1,2 +1,16 @@
 alert("Hello world");
 document.getElementById("main").innerHTML = "Hello world";
+
+let min = 1;
+let max = 10;
+let random = Math.floor(Math.random() * (max - min + 1)) + min;
+
+let index = 1;
+let str = '';
+while(index <= random){
+
+    str += index + ' ';
+    index++;
+}
+
+document.getElementById("number").innerHTML = str;

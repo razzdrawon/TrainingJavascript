c2b4b9ee848fcdebe9444ef2d571572850b626ef
Ricardo Axel Padilla Gonzalez <Ricardo.Padilla@walmart.com>
2018-08-20 12:04:57 -0500

    for with strings

--- labs/src/index.js
+++ labs/src/index.js
@@ -5,12 +5,51 @@ let min = 1;
 let max = 10;
 let random = Math.floor(Math.random() * (max - min + 1)) + min;
 
-let index = 1;
 let str = '';
-while(index <= random){
+for (let index = 1; index <= random; index++) {
 
-    str += index + ' ';
-    index++;
+    switch (index) {
+        case 1:
+            str += 'fun' + ' ';
+            break;
+
+        case 2:
+            str += 'blue' + ' ';
+            break;
+
+        case 3:
+            str += 'free' + ' ';
+            break;
+
+        case 4:
+            str += 'pour' + ' ';
+            break;
+
+        case 5:
+            str += 'hive' + ' ';
+            break;
+
+        case 6:
+            str += 'kicks' + ' ';
+            break;
+
+        case 7:
+            str += 'eleven' + ' ';
+            break;
+
+        case 8:
+            str += 'straight' + ' ';
+            break;
+
+        case 9:
+            str += 'fine' + ' ';
+            break;
+
+        case 10:
+            str += 'pen' + ' ';
+            break;
+    }
+
 }
 
 document.getElementById("number").innerHTML = str;

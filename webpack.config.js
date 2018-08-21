3effd500b60ad6623ff4b3ec90e8198cff98bc31
Ricardo Axel Padilla Gonzalez <Ricardo.Padilla@walmart.com>
2018-08-21 09:37:32 -0500

    webpack generator

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: "development",
-    entry: "./src/index.js",
+    entry: "./labs/src/intro/index.js",
     output: {
         path: path.resolve(__dirname, "dist"), // eslint-disable-line no-undef
         filename: "bundle.js"
     },
     module: {rules: []},
-    plugins: []
+    plugins: [new HtmlWebpackPlugin({template: './labs/src/intro/index.html'})]
 };

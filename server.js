bfad33c74c01abf5bc60b4429578c6459b666efd
Ricardo Axel Padilla Gonzalez <Ricardo.Padilla@walmart.com>
2018-08-21 11:50:26 -0500

    creating web server Node

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-// const path = require('path');
+const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; // eslint-disable-line no-process-env, no-undef, max-len
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/marco', (req, res) => {
-    return res.send("polo");
-});
+app.use(bodyParser.urlencoded({extended: true}));
+app.get('/marco', (req, res) => res.send("polo"));
+app.use(express.
+    static(path.join(__dirname, './dist'))); // eslint-disable-line no-undef
 app.listen(port, () => console.log(`Listening on port ${port}`));

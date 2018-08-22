5fe6e72ea2c65399e437aba650f86f1c1ba71711
Ricardo Axel Padilla Gonzalez <Ricardo.Padilla@walmart.com>
2018-08-22 09:59:57 -0500

    working with let and const

--- labs/src/intro/showHoisting.js
+++ labs/src/intro/showHoisting.js
@@ -1,16 +1,23 @@
 /* eslint func-style:off, require-jsdoc:off,
 space-before-function-paren:off, no-unused-vars:off,
-no-throw-literal:off, no-var:off, id-length:off*/
+no-throw-literal:off, no-var:off, id-length:off, sort-keys:off,
+newline-before-return:off*/
 function showHoisting() {
-    let x = 2;
+    const z = {
+        foo: true,
+        bar: true,
+        baz: true
+    };
+    z.foo = false;
+    z.qux = true;
+    const x = 2;
     if (x === 2) {
-        let y = true;
+        // x = 3;
+        const y = true;
         return y;
     }
-    else {
-        return false;
-    }
-    
+
+return false;
 }
 
 export default showHoisting;

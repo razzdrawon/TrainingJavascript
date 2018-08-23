57bab72373c87961af6e7e8d31e2717db2042802
Ricardo Axel Padilla Gonzalez <Ricardo.Padilla@walmart.com>
2018-08-23 11:44:52 -0500

    test throw error

--- spec/functions.spec.js
+++ spec/functions.spec.js
@@ -10,6 +10,21 @@ describe('functions', () => {
     it('will return true for arrow', () => {
         expect(arrow()).toBeTruthy();
     });
+    it('can create a person', () => {
+        let person = makePerson('Veronica', 'Castro', 'lavero@gmail.com', '55999999');
+        expect(person).toBe(JSON.stringify({firstName: 'Veronica', lastName: 'Castro', email: 'lavero@gmail.com', phone: '55999999'}));
+        return true;
+    });
+    it('will throw if no name is passed in', () => {
+        expect(() => makePerson(undefined, 'Castro', 'lavero@gmail.com', '55999999')).toThrowError();
+
+    });
+    it("will set email to 'no email' if not provided", () => {
+        return true;
+    });
+    it("will set phone to 'no phone' if not provided", () => {
+        return true;
+    });
 });
 
 
@@ -19,3 +34,10 @@ function statement () {return true;}
 
 const arrow = () => true;
 
+const makePerson = (name, last, email, phone) => {
+    if (name && last) {
+        let person = {firstName: name, lastName: last, email: email, phone: phone};
+        return JSON.stringify(person);
+    }
+    throw new Error("error");
+};

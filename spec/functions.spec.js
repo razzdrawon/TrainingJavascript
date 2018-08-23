24bfcfbaf5ece3cc17b57e8294806ff5759d8e97
Ricardo Axel Padilla Gonzalez <Ricardo.Padilla@walmart.com>
2018-08-23 11:47:57 -0500

    set email defualt

--- spec/functions.spec.js
+++ spec/functions.spec.js
@@ -13,20 +13,19 @@ describe('functions', () => {
     it('can create a person', () => {
         let person = makePerson('Veronica', 'Castro', 'lavero@gmail.com', '55999999');
         expect(person).toBe(JSON.stringify({firstName: 'Veronica', lastName: 'Castro', email: 'lavero@gmail.com', phone: '55999999'}));
-        return true;
     });
     it('will throw if no name is passed in', () => {
         expect(() => makePerson(undefined, 'Castro', 'lavero@gmail.com', '55999999')).toThrowError();
 
     });
     it("will set email to 'no email' if not provided", () => {
-        return true;
+        let person = makePerson('Veronica', 'Castro', undefined, '55999999');
+        expect(person).toBe(JSON.stringify({firstName: 'Veronica', lastName: 'Castro', email: 'no email', phone: '55999999'}));
     });
     it("will set phone to 'no phone' if not provided", () => {
         return true;
     });
 });
-
 
 const expression = function () {return true}
 
@@ -34,7 +33,7 @@ function statement () {return true;}
 
 const arrow = () => true;
 
-const makePerson = (name, last, email, phone) => {
+const makePerson = (name, last, email = 'no email', phone) => {
     if (name && last) {
         let person = {firstName: name, lastName: last, email: email, phone: phone};
         return JSON.stringify(person);
